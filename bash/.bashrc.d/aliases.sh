d229e05bba12327cc8a4edd4a151aa6a9a979863
Oliver H Page <ronbelina@gmail.com>
2026-04-21 18:25:02 +0100

    Update aliases.sh

--- bash/.bashrc.d/aliases.sh
+++ bash/.bashrc.d/aliases.sh
@@ -7,9 +7,24 @@ alias config="$EDITOR ~/.bashrc"
 alias aliases="$EDITOR ~/.bashrc.d/aliases.sh"
 alias functions="$EDITOR ~/.bashrc.d/functions.sh"
 
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
+alias reload='source ~/.bashrc'
 
+alias dir="dir --color=auto"
+alias vdir="vdir --color=auto"
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias egrep="egrep --color=auto"
+alias jctl="journalctl -p 3 -xb"
+alias psmem="ps auxf | sort -nr -k 4"
+alias psmem10="ps auxf | sort -nr -k 4 | head -10"
+alias h='history'
+alias hg='history | grep'
+alias df='df -h'
+alias du='du -h'
+alias free='free -h'
+
+# alias ls='ls --color=auto'
+# alias grep='grep --color=auto'
 alias ls="eza -al --color=always --group-directories-first --icons"
 alias la="eza -a --color=always --group-directories-first --icons"
 alias ll="eza -l --color=always --group-directories-first --icons"
@@ -20,18 +35,9 @@ alias b='btop'
 alias ff='fastfetch'
 alias c='clear'
 alias q='exit'
-alias tarhow="tar -acf"
-alias untar="tar -zxvf"
 alias wget="wget -c"
-alias psmem="ps auxf | sort -nr -k 4"
-alias psmem10="ps auxf | sort -nr -k 4 | head -10"
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-alias ......="cd ../../../../.."
-alias dir="dir --color=auto"
-alias vdir="vdir --color=auto"
-alias fgrep="fgrep --color=auto"
-alias egrep="egrep --color=auto"
-alias jctl="journalctl -p 3 -xb"
+
+alias ipkg='install_pkg'
+alias upkg='update_pkg'
+
+alias bigboi="ssh guggoo@BigBoi"
